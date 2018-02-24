2aedd9c23e123bff8a988ae8b7be04e1decf62f9
Ryan Gonzalez <rymg19@gmail.com>
2018-02-24 11:56:44 -0600

    beep boop

--- fbuildroot.py
+++ fbuildroot.py
@@ -78,6 +78,7 @@ def configure(ctx):
                             ({'clang'}, {'flags+': clang_flags}),
                        ], **kw)
 
+    print(clang_flags, nonclang_flags)
     cxx = guess_cxx.static(ctx, exe=ctx.options.cxx, flags=ctx.options.cxxflag,
                            platform_options=[
                             ({'posix'}, {'flags+': ['-std=c++11'] + posix_flags}),
